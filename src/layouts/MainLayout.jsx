7736f3aaa065ac27df90524be554961e6fc14fcb
Sai Praveen Naidu <saipraveennaidupudi@gmail.com>
2026-07-02 21:53:35 +0530

    feat: implement navigation bar structure

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,20 @@
 import { Outlet } from "react-router-dom";
 
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
 function MainLayout() {
-  return <Outlet />;
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import HeroSection from "./components/HeroSection/HeroSection";
+import SearchSection from "./components/SearchSection/SearchSection";
+import CategorySection from "./components/CategorySection/CategorySection";
+import NearbySection from "./components/NearbySection/NearbySection";
+import WomenFriendlySection from "./components/WomenFriendlySection/WomenFriendlySection";
+import BudgetSection from "./components/BudgetSection/BudgetSection";
+import BenefitsSection from "./components/BenefitsSection/BenefitsSection";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <HeroSection />
+      <SearchSection />
+      <CategorySection />
+      <NearbySection />
+      <WomenFriendlySection />
+      <BudgetSection />
+      <BenefitsSection />
+    </>
+  );
 }
 
 export default HomePage;
